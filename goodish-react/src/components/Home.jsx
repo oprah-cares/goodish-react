148a14c4b78ba1f2fed96ff0fe9cb254da51c4bc
kforks <kaitlynforks@gmail.com>
2024-06-19 15:54:43 -0500

    Add simple React home page component

--- goodish-react/src/components/Home.jsx
+++ goodish-react/src/components/Home.jsx
@@ -1,1 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Component</h1>
+        </div>
+    );
+};
+
+export default Home;
